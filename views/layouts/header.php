371a9d2b7687c73f189c5fb8e00fc1d5e86fe537
Pattounes <77328374+ldrt@users.noreply.github.com>
2021-03-15 15:22:59 +0100

    Structure

---
+++ views/layouts/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a href="" class="navbar-brand">Mon site</a>
+        </nav>
+
+        <div class="container mt-4">
+        
